c2c0766990d7dbd40bbfd2feab038f1707bdb4d6
yuvalrubin <yuval.rubin3@mail.huji.ac.il>
2025-08-30 12:32:51 +0300

    probably correct implementation up to thinning (which is currently not used at all)

--- 2007_model_stoch.py
+++ 2007_model_stoch.py
@@ -6,9 +6,10 @@ from simulating_36_n import Utils
 starting_position = 10
 tau_r = 1
 tau_s = 0.01
-dt = 0.001
+dt = 0.0001
 redIO = 1
-num_iterations = 10000
+num_iterations = 100
+highest_firing_rate = 100  # in Hz
 np.random.seed(7)
 
 # ---------- saccade mechanism  ----------
@@ -61,6 +62,8 @@ eye_position[:, 0] = np.dot((SR[:, 0] - SL[:, 0]), Utils.eta)
 
 # ---------- run simulation  ----------
 
+see_vals = np.zeros((len(Utils.xi), num_iterations))  # For debugging: to see rR*dt values
+
 for i in range(1, num_iterations):
 
     if i % int(0.5 / dt) == 0:  # Every 5 seconds (converted to steps)
@@ -84,15 +87,20 @@ for i in range(1, num_iterations):
     
     # calculate spikes
 
-    add_to_spike_cnt_R = np.random.rand(len(Utils.xi)) < (rR[:, 0] * dt * redIO)
-    add_to_spike_cnt_L = np.random.rand(len(Utils.xi)) < (rL[:, 0] * dt * redIO)
+    # add_to_spike_cnt_R = np.random.rand(len(Utils.xi)) < (rR[:, i] * dt * redIO*100)
+    # add_to_spike_cnt_L = np.random.rand(len(Utils.xi)) < (rL[:, i] * dt * redIO*100)
 
-    spike_cnt_R += add_to_spike_cnt_R
-    spike_cnt_L += add_to_spike_cnt_L
+    # spike_cnt_R += add_to_spike_cnt_R
+    # spike_cnt_L += add_to_spike_cnt_L
 
-    spiking_R[:, 0] = spike_cnt_R % redIO == 0
-    spiking_L[:, 0] = spike_cnt_L % redIO == 0
+    # spiking_R[:, i] = (redIO != 0 and spike_cnt_R % redIO == 0)
+    # spiking_L[:, i] = (redIO != 0 and spike_cnt_L % redIO == 0)
+    # spike_cnt_R[spiking_R[:, i] == 1] -= redIO
+    # spike_cnt_L[spiking_L[:, i] == 1] -= redIO
     
+    spiking_R[:, i] = np.random.rand(len(Utils.xi)) < (dt * rR[:, i])
+    spiking_L[:, i] = np.random.rand(len(Utils.xi)) < (dt * rL[:, i])
+    see_vals[:, i] = dt * rL[:, i]
     rR_activated = Utils.traditional_by_spike(rR[:, i], spiking_R[:, i])
     rL_activated = Utils.traditional_by_spike(rL[:, i], spiking_L[:, i])
 
@@ -121,22 +129,31 @@ plt.show()
 
 # ---------- plot rR for a specific neuron ----------
 
-# neuron_index = 15  # Choose which neuron to plot (adjust as needed)
-# plt.figure(figsize=(10, 6))
-# plt.plot(time, rR[neuron_index, :])
-# plt.xlabel('Time (s)')
-# plt.ylabel(f'rR[{neuron_index}]')
-# plt.title(f'Right Side Neuron {neuron_index} Activity Over Time')
-# plt.grid(True)
-# plt.show()
+neuron_index = 15  # Choose which neuron to plot (adjust as needed)
+plt.figure(figsize=(10, 6))
+plt.plot(time, rR[neuron_index, :])
+plt.xlabel('Time (s)')
+plt.ylabel(f'rR[{neuron_index}]')
+plt.title(f'Right Side Neuron {neuron_index} Activity Over Time')
+plt.grid(True)
+plt.show()
 
 # ---------- plot spiking_R for a specific neuron ----------
 
-neuron_index = 15  # Choose which neuron to plot (adjust as needed)
 plt.figure(figsize=(10, 6))
-plt.plot(time, spiking_R[neuron_index, :], 'o-', markersize=2)
+spike_counts = np.sum(spiking_R, axis=0)
+plt.plot(time, spike_counts)
 plt.xlabel('Time (s)')
-plt.ylabel(f'spiking_R[{neuron_index}]')
-plt.title(f'Right Side Neuron {neuron_index} Spiking Activity Over Time')
+plt.ylabel('Number of Spiking Neurons')
+plt.title('Number of Right Side Neurons Spiking Over Time')
+plt.grid(True)
+plt.show()
+
+# ---------- plot see_vals for a specific neuron ----------
+plt.figure(figsize=(10, 6))
+plt.plot(time, see_vals[neuron_index, :])
+plt.xlabel('Time (s)')
+plt.ylabel(f'rR[{neuron_index}]*dt')
+plt.title(f'Right Side Neuron {neuron_index} rR*dt Over Time')
 plt.grid(True)
 plt.show()
